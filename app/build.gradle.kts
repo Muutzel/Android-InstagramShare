2c368d4e1d874ad9b910d14a3dfdfe7513333821
Muutzel <muutzel@googlemail.com>
2025-08-04 20:36:05 +0200

    add qr code generating variante 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -56,5 +55,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
+    implementation("com.lightspark:compose-qr-code:1.0.1")
 }
--- app/src/main/java/com/example/instagramshare/MainActivity.kt
+++ app/src/main/java/com/example/instagramshare/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.instagramshare
 
+import android.content.Context
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -16,8 +17,13 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,13 +34,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.instagramshare.ui.theme.InstagramShareTheme
+import com.lightspark.composeqr.DotShape
+import com.lightspark.composeqr.QrCodeColors
+import com.lightspark.composeqr.QrCodeView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -69,7 +77,6 @@ fun ContentInSurface() {
     //Daten initial holen
     val usernameInitial = readInfosOnPhone(context).username
     val bioInitial = readInfosOnPhone(context).bio
-    val imageQr = painterResource(R.drawable.marcel_892__qr_orange)
 
     //Daten in Variable speichern, die sich mit der Zeit verändern
     var username by rememberSaveable { mutableStateOf(usernameInitial) }
@@ -97,14 +104,24 @@ fun ContentInSurface() {
             horizontalAlignment = Alignment.CenterHorizontally // horizontale Zentrierung
         ) {
             //qrCode als Bild
-            Image(
-                painter = imageQr,
-                contentDescription = null,
-                contentScale = ContentScale.Fit,
-                modifier = Modifier
-                    .height(230.dp)
-                    .clip(RoundedCornerShape(16.dp))
-            )
+            GenerateQrCode(username,context)
+//            Image(
+//                painter = rememberQrCodePainter(
+//                    getInstagramLink(username,context),
+//                    shapes = QrShapes(
+//                        darkPixel = QrPixelShape.roundCorners() // für runde "Punkte"
+//                    ),
+//                    colors = QrColors(
+//                        dark = QrBrush.solid(Color.Magenta),
+//                        light = QrBrush.solid(Color.White)
+//                    )
+//                ),
+//                contentDescription = null,
+//                contentScale = ContentScale.Fit,
+//                modifier = Modifier
+//                    .height(230.dp)
+//                    //.clip(RoundedCornerShape(16.dp))
+//            )
 
             //unsichtbare Trennung
             Spacer(modifier = Modifier.height(50.dp))
@@ -135,14 +152,15 @@ fun ContentInSurface() {
                         .fillMaxHeight(),
                     contentAlignment = Alignment.Center
                 ) {
-                    Image(
-                        painter = imageQr,
-                        contentDescription = null,
-                        contentScale = ContentScale.Fit,
-                        modifier = Modifier
-                            .height(230.dp)
-                            .clip(RoundedCornerShape(16.dp))
-                    )
+                    GenerateQrCode(username,context)
+//                    Image(
+//                        painter = imageQr,
+//                        contentDescription = null,
+//                        contentScale = ContentScale.Fit,
+//                        modifier = Modifier
+//                            .height(230.dp)
+//                            .clip(RoundedCornerShape(16.dp))
+//                    )
                 }
 
                 Box(
@@ -162,4 +180,42 @@ fun ContentInSurface() {
             }
         }
     }
+}
+
+@Composable
+fun GenerateQrCode(username: String, context: Context) {
+    Box(
+        modifier = Modifier
+            .size(300.dp)
+            .clip(RoundedCornerShape(24.dp)) // Rundung an den Kanten, z.B. 24.dp
+            .background(Color.Black),
+        contentAlignment = Alignment.Center
+    )
+    {
+        QrCodeView(
+            data = getInstagramLink(username,context),
+            modifier = Modifier.size(260.dp),
+            colors = QrCodeColors(
+                background = Color.Black,
+                foreground = Color.White
+            ),
+            dotShape = DotShape.Circle
+        )
+//        {
+//            Box(
+//                modifier = Modifier
+//                    .size(56.dp) // Größe des leeren Bereichs
+//                    .background(Color.White) // Oder Color.Transparent für transparenten Bereich
+//            )
+//        }
+    }
+//
+    //                    Image(
+//                        painter = imageQr,
+//                        contentDescription = null,
+//                        contentScale = ContentScale.Fit,
+//                        modifier = Modifier
+//                            .height(230.dp)
+//                            .clip(RoundedCornerShape(16.dp))
+//                    )
 }
