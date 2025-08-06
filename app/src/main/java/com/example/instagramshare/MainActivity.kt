caef35317af45efcc12b0f022f7466f030ef69bb
Muutzel <muutzel@googlemail.com>
2025-08-06 21:10:55 +0200

    adjustment of color in qr code and line

--- app/src/main/java/com/example/instagramshare/MainActivity.kt
+++ app/src/main/java/com/example/instagramshare/MainActivity.kt
@@ -34,7 +34,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.instagramshare.ui.theme.InstagramShareTheme
-import android.net.Uri
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.draw.clip
